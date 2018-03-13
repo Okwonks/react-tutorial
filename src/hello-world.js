096d9edb1428e67a0d4ab7057a9af43efd08f3ae
Okwonks <albertoketch@gmail.com>
2018-03-13 17:04:38 +0300

    Adding friends to the list of friends.

--- src/hello-world.js
+++ src/hello-world.js
@@ -25,11 +25,65 @@ import ReactDOM from 'react-dom';
 //     }
 // });
 
+class ShowList extends React.Component {
+    render() {
+        let listItems = this.props.names.map(function (friend) {
+            return <li>{friend}</li>
+        });
+        return (
+            <div>
+                <h4>Friends</h4>
+                <ul>
+                    {/* {this.props.names.map((friend) => <li>{friend}</li>)} this is an alternative way */}
+                    {listItems}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class AddFriends extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newFriend: ''
+        }
+
+        this.updateNewFriend = this.updateNewFriend.bind(this);
+        this.handleAddNew = this.handleAddNew.bind(this);
+    }
+    updateNewFriend(e) {
+        this.setState({
+            newFriend: e.target.value
+        })
+    }
+    handleAddNew = () => {
+        this.props.addNew(this.state.newFriend);
+        this.setState({
+            newFriend: ''
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend} />
+                <button onClick={this.handleAddNew}>Add Friend</button>
+            </div>
+        )
+    }
+}
+
 class UserName extends React.Component {
     constructor(props) {
         super(props);
         this.state = UserName.defaultProps;
         this.handleChange = this.handleChange.bind(this);
+        this.addFriend = this.addFriend.bind(this);
+    }
+    addFriend(friend) {
+        this.setState((state) => ({
+            friends: state.friends.concat([friend])
+        }))
     }
     handleChange = (e) => {
         this.setState({
@@ -45,15 +99,18 @@ class UserName extends React.Component {
     render() {
         return (
             <div>
-                Hello {this.state.username} <br/>
-                Change Name: <input type="text" value={this.state.username} onChange={this.handleChange}/>
+                <h3>Hello {this.state.username}</h3> <br />
+                <ShowList names={this.state.friends} />
+                Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+                <AddFriends addNew={this.addFriend}/>
             </div>
         )
     }
 }
 
 UserName.defaultProps = {
-    username: '@albert'
+    username: '@albert',
+    friends: ['Jake Lingwall', 'Murphy Randall', 'Merrick Christensen', 'Drake the Winstone']
 }
 
 ReactDOM.render(<UserName />, document.getElementById('hello-user'));
